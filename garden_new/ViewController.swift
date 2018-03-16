07d8ecce63028dbbb45cd6b961eaa3a557447197
shin04 <daikon0413@gmail.com>
2018-03-16 09:55:23 +0900

    Initial Commit

---
+++ garden_new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garden_new
+//
+//  Created by 梶原大進 on 2018/03/16.
+//  Copyright © 2018年 梶原大進. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ garden_newTests/garden_newTests.swift
@@ -0,0 +1,36 @@
+//
+//  garden_newTests.swift
+//  garden_newTests
+//
+//  Created by 梶原大進 on 2018/03/16.
+//  Copyright © 2018年 梶原大進. All rights reserved.
+//
+
+import XCTest
+@testable import garden_new
+
+class garden_newTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
